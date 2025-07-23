3bf2db7cd36c814099c3e56521abcfff06860b58
Advika Bhosle <advikabhosle@gmail.com>
2025-07-23 22:43:34 +0530

    iteration2

---
+++ iteration2.cpp
@@ -0,0 +1,35 @@
+// Name - Advika Bhosle
+// PRN - 24070123007
+// Batch - EnTC - A1
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    for(int i = 1; i <= 5; i++) {
+        cout << "PROGRAMMING IS FUN" << endl;
+    }
+
+    for(int i = 1; i <= 5; i++) {
+        cout << "CODING NEEDS SKILLS" << endl;
+    }
+
+    return 0;
+}
+
+//output
+/*
+PROGRAMMING IS FUN
+PROGRAMMING IS FUN
+PROGRAMMING IS FUN
+PROGRAMMING IS FUN
+PROGRAMMING IS FUN
+CODING NEEDS SKILLS
+CODING NEEDS SKILLS
+CODING NEEDS SKILLS
+CODING NEEDS SKILLS
+CODING NEEDS SKILLS
+
+
+=== Code Execution Successful ===
+/*
