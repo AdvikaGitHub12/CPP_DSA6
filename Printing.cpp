ace787bc9d05d71831d773edd616cc54b1c1b656
Advika Bhosle <advikabhosle@gmail.com>
2025-07-29 23:09:31 +0530

    Add files via upload

---
+++ Printing.cpp
@@ -0,0 +1,18 @@
+// Advika Bhosale
+// 24070123007
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<5;i++)
+    {cout<<"Advika Bhosale"<<endl;}
+    return 0;
+}
+
+// OUTPUT
+// Advika Bhosale
+// Advika Bhosale
+// Advika Bhosale
+// Advika Bhosale
+// Advika Bhosale
